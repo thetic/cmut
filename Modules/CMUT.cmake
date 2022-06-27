466e1d35bbbf6733a8e1f07fbf4330e4b6b796d0
Chad Condon <chad@condon.tech>
2022-06-26 17:47:24 -0700

    Create add_cmut_test_project function

--- Modules/CMUT.cmake
+++ Modules/CMUT.cmake
@@ -1,6 +1,56 @@
 cmake_policy(PUSH)
 cmake_policy(VERSION 3.18)
 
+function(add_cmut_test_project)
+    # Parse arguments
+    set(options)
+    set(oneValueArgs NAME SOURCE_DIR)
+    set(multiValueArgs CMAKE_FLAGS)
+    cmake_parse_arguments(
+        PARSE_ARGV 0
+        CMUT
+        "${options}"
+        "${oneValueArgs}"
+        "${multiValueArgs}"
+    )
+
+    # Validate arguments
+    if(CMUT_UMPARSED_ARGUMENTS)
+        string(JOIN "\n\t" split CMUT_UMPARSED_ARGUMENTS)
+        message(FATAL_ERROR
+            "add_cmut_test_project given unknown argument:\n\n\t${split}"
+        )
+    elseif(NOT CMUT_NAME)
+        message(FATAL_ERROR "add_cmut_test_project must be given non-empty NAME.")
+    elseif(NOT CMUT_SOURCE_DIR)
+        message(FATAL_ERROR "add_cmut_test_project must be given non-empty SOURCE_DIR.")
+    endif()
+
+    get_filename_component(source_dir
+        ${CMUT_SOURCE_DIR} ABSOLUTE
+        BASE_DIR ${CMAKE_CURRENT_SOURCE_DIR}
+    )
+    if(NOT EXISTS "${source_dir}")
+        message(FATAL_ERROR
+            "The source directory \"${CMUT_SOURCE_DIR}\" does not exist."
+        )
+    elseif(NOT EXISTS "${source_dir}/CMakeLists.txt")
+        message(FATAL_ERROR
+            "The source directory \"${CMUT_SOURCE_DIR}\" does not contain a CMakeLists.txt file."
+        )
+    endif()
+
+    add_test(
+        NAME ${CMUT_NAME}
+        COMMAND
+            ${CMAKE_COMMAND}
+            -S "${source_dir}"
+            -B "${CMAKE_CURRENT_BINARY_DIR}/cmut/${CMUT_NAME}"
+            -D "CMAKE_MODULE_PATH=${CMAKE_MODULE_PATH}"
+            ${CMUT_CMAKE_FLAGS}
+    )
+endfunction()
+
 function(assert condition)
     message(CHECK_START "${condition}")
     cmake_language(EVAL
