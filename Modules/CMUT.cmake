96e5b4076b7bf3daf8bd6175c98879ab77a4e879
Chad Condon <chad@condon.tech>
2022-06-26 17:15:42 -0700

    Create assert function

--- Modules/CMUT.cmake
+++ Modules/CMUT.cmake
@@ -1,4 +1,16 @@
 cmake_policy(PUSH)
 cmake_policy(VERSION 3.18)
 
+function(assert condition)
+    message(CHECK_START "${condition}")
+    cmake_language(EVAL
+        CODE "
+            if(NOT (${condition}))
+                message(FATAL_ERROR [=[Failed assertion: ${condition}]=])
+            endif()
+        "
+    )
+    message(CHECK_PASS "Pass")
+endfunction()
+
 cmake_policy(POP)
